c10030784aa2b651663b6a4ced96d4e078fc2fee
Jayson Powers <japow75@gmail.com>
2024-10-04 11:08:37 -0400

    initial first test.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+// p5.js setup function
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(400, 400);
+    let arrow1 = new Arrow(createVector(50, 200), createVector(200, 200));
+    let arrow2 = new Arrow(createVector(200, 200), createVector(350, 200));
+    let composedArrow = arrow1.compose(arrow2);
+    arrow1.display();
+    arrow2.display();
+    composedArrow.display();
 }
 
+// p5.js draw function
 function draw() {
-  background(220);
+    // No continuous drawing needed
+    noLoop();
 }
