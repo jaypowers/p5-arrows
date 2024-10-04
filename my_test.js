ec06236cd8f29a1a4803ab16fb007adfe6edd9c4
Jayson Powers <japow75@gmail.com>
2024-10-04 11:32:26 -0400

    my_test.js not currently functioning the way I want it to

---
+++ my_test.js
@@ -0,0 +1,23 @@
+// my_test.js
+
+// create a simple loging system for me, the developer
+function log(message) {
+    console.log(message);
+}
+
+// create a simple test class that logs and accepts arguments to test as well
+class Test {
+    constructor(name) {
+        this.name = name;
+    }
+
+    test(arg1, arg2) {
+        log(`Testing ${this.name}`);
+        // equality test
+        if (arg1 === arg2) {
+            log(`Test passed!`);
+        } else {
+            log(`Test failed!`);
+        }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,17 +1,30 @@
 // p5.js setup function
+let arrow1, arrow2;
+let composedArrow;
+let test;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    let arrow1 = new Arrow(createVector(50, 200), createVector(200, 200));
-    let arrow2 = new Arrow(createVector(200, 200), createVector(350, 200));
-    let composedArrow = arrow1.compose(arrow2);
-    arrow1.display();
-    arrow2.display();
-    composedArrow.display();
+    arrow1 = new Arrow(createVector(50, 200), createVector(200, 200));
+    arrow2 = new Arrow(createVector(200, 200), createVector(350, 200));
+    composedArrow = arrow1.compose(arrow2);
 }
 
 // p5.js draw function
 function draw() {
   background(51);
+  fill(255);
+  stroke(0);
+  arrow1.display();
+  arrow2.display();
+  composedArrow.display();
+
+  // my_test.js
+
+  // create a new test object
+  test = new Test('Equality test');
+  test.test(1, 1);  // Test passed!
+  
   // No continuous drawing needed
   noLoop();
 }
